57e40dfbcf0969739abbd81f84f6875ef8d8f992
Yakov Kulikov <yahakulikov@yandex.ru>
2025-10-22 17:35:38 +0500

    chore: add project foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import App from "./App";
 import './index.css'
-import App from './App.tsx'
+import 'virtual:uno.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <PrimeReactProvider>
+            <App />
+        </PrimeReactProvider>
+    </React.StrictMode>
+);
